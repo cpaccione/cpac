1c4c5dd5472701928e6889f4147ac5a3570e2554
Frost <cpaccione77@gmail.com>
2016-03-09 16:55:41 -0600

    Removed commented out JavaScript and duplicate CSS

--- js/script.js
+++ js/script.js
@@ -11,29 +11,4 @@ jQuery(document).ready(function($) {
         trigger: 'hover'
       }); 
 
-	//Resize and load function to add padding to push content below bootstrap fixed navbar
-
-	// $(window).resize(function () { 
-	//     $('body').css('padding-top', parseInt($('.navbar').css("height"))+0);
-	// });
-
-	// $(window).load(function () { 
-	//     $('body').css('padding-top', parseInt($('.navbar').css("height"))+0);        
-	// });
-
-    // $("div.portfolio-wrap").mouseenter(function() {
-    //     $("img.portfolio-image").fadeOut();
-    // });
-
-    // $("div.portfolio-wrap").mouseleave(function() {
-    // 	$("img.portfolio-image").fadeIn();
-    // })
-
-    // $(".portfolio-wrap").hover(function() {
-    //     $("img.portfolio-image").fadeIn();
-    // });
-
-
-
-
 });
