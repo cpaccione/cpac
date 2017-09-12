d678bc5db480263a1d7f13ec71419fb76c19756e
Chris Paccione <cpaccione77@gmail.com>
2017-09-12 13:01:55 -0500

    Portfolio updates

--- page-portfolio-template.php
+++ page-portfolio-template.php
@@ -17,73 +17,67 @@ $counter = 0;
 
 <?php get_header(); ?>
 
-<section class="header d-flex align-items-center">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12 text-center">
-                <?php the_title('<h1>', '</h1>'); ?>
+    <section class="header d-flex align-items-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center">
+                    <?php the_title('<h1>', '</h1>'); ?>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
+    <section class="portfolio-section">
+        <div class="container-fluid">
 
+        <?php
 
-<!-- <section class="page-portfolio"> -->
-<section class="portfolio-section">
-    <div class="container-fluids">
+                $args = array(
+                    'post_type' => 'portfolio',
+                    'post_status' => 'publish',
+                    'posts_per_page' => -1,
+                    'orderby' => 'date',
+                    'order' => 'DESC'
+                    );
+                $query = new WP_Query($args);
 
-    <?php
+            ?>
 
-            $args = array(
-                'post_type' => 'portfolio',
-                'post_status' => 'publish',
-                'posts_per_page' => -1,
-                'orderby' => 'date',
-                'order' => 'DESC'
-                );
-            $query = new WP_Query($args);
+                <?php if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
 
-        ?>
+                    <?php if($counter % 3 === 0) : echo '<div class="row">'; endif; ?>
 
-            <?php if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
+                        <div class="col-md-4 animation-element bounce-up">
+                            <a href="<?php the_permalink(); ?>">
+                                <div class="subject img-overlay">
 
-                <?php if($counter % 3 === 0) : echo '<div class="row">'; endif; ?>
+                                    <?php
 
-                    <div class="col-md-4 animation-element bounce-up">
-                        <a href="<?php the_permalink(); ?>">
-                            <div class="subject img-overlay">
+                                      if ( has_post_thumbnail() ) {
+                                        the_post_thumbnail('large');
+                                      }
 
-                                <?php
+                                    ?>
 
-                                  if ( has_post_thumbnail() ) {
-                                    the_post_thumbnail('large');
-                                  }
-
-                                ?>
-
-                                <div class="portfolio-overlay">
-                                    <div class="overlay-copy">
-                                        <div class="center">
-                                            <span class="underline">
-                                                <h5><?php the_title(); ?></h5>
-                                            </span>
+                                    <div class="portfolio-overlay">
+                                        <div class="overlay-copy">
+                                            <div class="center">
+                                                <span class="underline">
+                                                    <h5><?php the_title(); ?></h5>
+                                                </span>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </a>
-                    </div>
+                            </a>
+                        </div>
 
-                <?php $counter++; if($counter % 3 === 0) : echo '</div>'; endif; ?>
+                    <?php $counter++; if($counter % 3 === 0) : echo '</div>'; endif; ?>
 
-            <?php endwhile; endif; wp_reset_postdata(); ?>
-
-
-    </div>
-</section>
+                <?php endwhile; endif; wp_reset_postdata(); ?>
 
 
-
+        </div>
+    </section>
 
 <?php get_footer(); ?>
