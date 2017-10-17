710a7194eddaed082c20760d5a63f81d6357cec9
Chris Paccione <cpaccione77@gmail.com>
2017-10-17 00:02:52 -0500

    Home page
    I added Bootstrap 4, and removed the hero image from the home page, and simplified the footer.

--- footer.php
+++ footer.php
@@ -1,3 +1,18 @@
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<hr>
+						<ul class="social-media">
+							<li><a href="https://github.com/cpaccione" target="_blank"><i class="fa fa-github fa-3x"></i></a></li>
+							<li><a href="https://www.linkedin.com/in/cpaccione" target="_blank"><i class="fa fa-linkedin fa-3x"></i></a></li>
+							<li><a href="/contact/"><i class="fa fa-envelope-o fa-3x" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</footer>
+
 		<?php wp_footer(); ?>
 	</body>
 </html>
--- page-home.php
+++ page-home.php
@@ -13,33 +13,35 @@ $counter = 0;
 
 <?php get_header(); ?>
 
-  <section class="home-intro">
-    <div class="cpac-wrap">
-
-    <div class="row">
-      <div class="col6">
-        <h1>Front-end and WordPress Development</h1>
-        <small style="text-align: center;">HTML<i class="fa fa-minus"></i> CSS <i class="fa fa-minus"></i> jQuery <i class="fa fa-minus"></i> WordPress</small>
-        <p>Hi, I'm Chris Paccione, a front-end web developer motivated by a passion for coding and WordPress. I offer front-end development, WordPress theme building, and HTML e-mail marketing services. Please view my work below and contact me if your ready to get started or want to discuss your next project.</p>
-      </div>
-      <div class="col6">
-        <img class="circle" src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/chris_paccione_500.jpg" alt="Chris Paccione front-end web developer">
-        <ul class="social-media">
-          <li><a href="https://github.com/cpaccione" target="_blank"><i class="fa fa-github fa-4x"></i></a></li>
-          <li><a href="https://www.linkedin.com/in/cpaccione" target="_blank"><i class="fa fa-linkedin fa-4x"></i></a></li>
-        </ul>
-        <div class="button-wrap">
-          <a href="/contact/"><button type="button" class="btn btn-primary btn-lg active">Contact Me</button></a>
+  <section class="home-hero">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h1>Front-end and WordPress Development</h1>
+          <small style="text-align: center;">HTML <i class="fa fa-minus"></i> CSS <i class="fa fa-minus"></i> jQuery <i class="fa fa-minus"></i> WordPress</small>
+          <p class="lead">Hi, I'm Chris Paccione, a front-end web developer motivated by a passion for coding and WordPress. I offer front-end development, WordPress theme building, and HTML e-mail marketing services. Please view my work below and contact me if your ready to get started or want to discuss your next project.</p>
+          <ul class="social-media">
+            <li><a href="https://github.com/cpaccione" target="_blank"><i class="fa fa-github fa-3x"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/cpaccione" target="_blank"><i class="fa fa-linkedin fa-3x"></i></a></li>
+            <li><a href="/contact/"><i class="fa fa-envelope-o fa-3x" aria-hidden="true"></i></a></li>
+          </ul>
+        </div>
+        <div class="col-md-6">
+          <img class="circle" src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/chris_paccione_500.jpg" alt="Chris Paccione front-end web developer">
         </div>
       </div>
-    </div>
-
-
     </div>
   </section>
 
   <section class="portfolio-section">
-      <div class="container-fluid">
+    <div class="container">
+      
+      <div class="row">
+        <div class="col-12">
+          <h2>Portfolio</h2>
+        </div>
+      </div>
+
 
       <?php
 
@@ -58,9 +60,9 @@ $counter = 0;
 
                   <?php if($counter % 3 === 0) : echo '<div class="row">'; endif; ?>
 
-                      <div class="col6">
+                      <div class="col-md-4">
                           <a href="<?php the_permalink(); ?>">
-                              <!-- <div class="subject img-overlay"> -->
+                              <div class="subject img-overlay">
 
                                   <?php
 
@@ -70,24 +72,23 @@ $counter = 0;
 
                                   ?>
 
-                                  <!-- <div class="portfolio-overlay"> -->
-                                      <!-- <div class="overlay-copy"> -->
-                                          <!-- <div class="center"> -->
+                                  <div class="portfolio-overlay">
+                                      <div class="overlay-copy">
+                                          <div class="center">
                                               <span class="underline">
                                                   <h5><?php the_title(); ?></h5>
                                               </span>
-                                          <!-- </div> -->
-                                      <!-- </div> -->
-                                  <!-- </div> -->
-                              <!-- </div> -->
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
                           </a>
                       </div>
 
                   <?php $counter++; if($counter % 3 === 0) : echo '</div>'; endif; ?>
 
               <?php endwhile; endif; wp_reset_postdata(); ?>
-
-      </div>
+    </div>
   </section>
 
 
