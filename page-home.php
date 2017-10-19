c5b2516ca38ff2e842b8b678186297d39d5011a6
Frost <cpaccione77@gmail.com>
2017-10-18 22:51:29 -0500

    Home page
    Column updates

--- page-home.php
+++ page-home.php
@@ -16,7 +16,7 @@ $counter = 0;
   <section class="home-hero">
     <div class="container">
       <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-7">
           <h1>Front-end and WordPress Development</h1>
           <small style="text-align: center;">HTML <i class="fa fa-minus"></i> CSS <i class="fa fa-minus"></i> jQuery <i class="fa fa-minus"></i> WordPress</small>
           <p class="lead">Hi, I'm Chris Paccione, a front-end web developer motivated by a passion for coding and WordPress. I offer front-end development, WordPress theme building, and HTML e-mail marketing services. Please view my work below and contact me if your ready to get started or want to discuss your next project.</p>
@@ -26,7 +26,7 @@ $counter = 0;
             <li><a href="/contact/"><i class="fa fa-envelope-o fa-3x" aria-hidden="true"></i></a></li>
           </ul>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-5">
           <img class="circle" src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/chris_paccione_500.jpg" alt="Chris Paccione front-end web developer">
         </div>
       </div>
@@ -35,7 +35,7 @@ $counter = 0;
 
   <section class="portfolio-section">
     <div class="container">
-      
+
       <div class="row">
         <div class="col-12">
           <h2>Portfolio</h2>
