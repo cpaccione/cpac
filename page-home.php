8bedee31e8a04678245c78941438e19be21adc8a
Chris Paccione <cpaccione77@gmail.com>
2017-10-19 01:52:43 -0500

    Home/about page
    Cleaned up and adjusted CSS for home page, and added the start of the about page template.

--- page-home.php
+++ page-home.php
@@ -17,7 +17,7 @@ $counter = 0;
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h1>WordPress &amp; Front-end Developer</h1>
+                    <h1>WordPress &amp; <span class="no-wrap">Front-end Developer</span></h1>
                     <p>I focus on clean code, quality responsive sites, and a great user experience</p>
                     <small>HTML <i class="fa fa-minus"></i> CSS <i class="fa fa-minus"></i> jQuery <i class="fa fa-minus"></i> WordPress</small>
                     <a href="#portfolio-link" class="smooth btn-primary active">View Portfolio</a>
@@ -25,39 +25,6 @@ $counter = 0;
             </div>
         </div>
     </section>
-
-<!--   <section class="home-hero">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-7">
-          <h1>Front-end and WordPress Development</h1>
-          <small style="text-align: center;">HTML <i class="fa fa-minus"></i> CSS <i class="fa fa-minus"></i> jQuery <i class="fa fa-minus"></i> WordPress</small>
-          <p class="lead">Hi, I'm Chris Paccione, a front-end web developer motivated by a passion for coding and WordPress. I offer front-end development, WordPress theme building, and HTML e-mail marketing services. Please view my work below and contact me if your ready to get started or want to discuss your next project.</p>
-          <ul class="social-media">
-            <li><a href="https://github.com/cpaccione" target="_blank"><i class="fa fa-github fa-3x"></i></a></li>
-            <li><a href="https://www.linkedin.com/in/cpaccione" target="_blank"><i class="fa fa-linkedin fa-3x"></i></a></li>
-            <li><a href="/contact/"><i class="fa fa-envelope-o fa-3x" aria-hidden="true"></i></a></li>
-          </ul>
-        </div>
-        <div class="col-md-5">
-          <img class="circle" src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/chris_paccione_500.jpg" alt="Chris Paccione front-end web developer">
-        </div>
-      </div>
-    </div>
-<<<<<<< HEAD
-  </section> -->
-=======
-  </section>
-
-  <section class="portfolio-section">
-    <div class="container">
-
-      <div class="row">
-        <div class="col-12">
-          <h2>Portfolio</h2>
-        </div>
-      </div>
->>>>>>> c5b2516ca38ff2e842b8b678186297d39d5011a6
 
   <section id="portfolio-link" class="portfolio-section">
     <div class="container-fluid">
@@ -79,7 +46,7 @@ $counter = 0;
 
                   <?php if($counter % 3 === 0) : echo '<div class="row">'; endif; ?>
 
-                      <div class="col-md-4">
+                      <div class="col-sm-4">
                           <a href="<?php the_permalink(); ?>">
                               <div class="subject img-overlay">
 
@@ -120,7 +87,7 @@ $counter = 0;
       </div>
   </section>
 
-  <a href="#"><button type="button" class="btn btn-primary btn-lg btn-block"><h3>ABOUT ME &nbsp;&nbsp;  <i class="fa fa-arrow-circle-o-right fa-lg" aria-hidden="true"></i></h3></button></a>
+  <a href="#"><button type="button" class="btn btn-primary btn-lg btn-block"><h3>ABOUT ME <i class="fa fa-arrow-circle-o-right fa-lg" aria-hidden="true"></i></h3></button></a>
 
 
 <?php get_footer(); ?>
