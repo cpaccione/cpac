48a39e0cad60119fa15c0483de7ba546630f5e4a
Chris Paccione <cpaccione77@gmail.com>
2018-02-02 00:27:38 -0600

    Code formatting

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -1,28 +1,25 @@
 <?php get_header(); ?>
 
 
-<div class="container">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
 
-    <div class="row">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-        <div class="col-md-12">
+                <h3><?php the_title(); ?></h3>
+                    <?php the_content(); ?>
 
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php endwhile; else: ?>
 
-            <h3><?php the_title(); ?></h3>
-                <?php the_content(); ?>
+                <h1>Oh no!</h1>
+                <p>No content is appearing on this page!</p>
 
-        <?php endwhile; else: ?>
+            <?php endif; ?>
 
-            <h1>Oh no!</h1>
-            <p>No content is appearing on this page!</p>
-
-        <?php endif; ?>
-
+            </div>
         </div>
     </div>
-</div>
-
 
 
 <?php get_footer(); ?>
