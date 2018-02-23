23c3a8390c212aff2c9afb446ebce602739873e4
Chris Paccione <cpaccione77@gmail.com>
2018-02-23 01:44:41 -0600

    Added home page

--- page-home.php
+++ page-home.php
@@ -28,57 +28,56 @@ $counter = 0;
         </div>
     </section>
 
-  <section id="portfolio-link" class="portfolio-section">
-    <div class="container-fluid">
+    <section id="portfolio-link" class="portfolio-section">
+        <div class="container-fluid">
+        <?php
 
-      <?php
+                $args = array(
+                    'post_type' => 'portfolio',
+                    'post_status' => 'publish',
+                    'portfolio_category' => 'featured',
+                    'posts_per_page' => 6,
+                    'orderby' => 'date',
+                    'order' => 'DESC'
+                    );
+                $query = new WP_Query($args);
 
-              $args = array(
-                  'post_type' => 'portfolio',
-                  'post_status' => 'publish',
-                  'portfolio_category' => 'featured',
-                  'posts_per_page' => 6,
-                  'orderby' => 'date',
-                  'order' => 'DESC'
-                  );
-              $query = new WP_Query($args);
+            ?>
 
-          ?>
+            <?php if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
 
-              <?php if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
+                <?php if($counter % 3 === 0) : echo '<div class="row">'; endif; ?>
 
-                  <?php if($counter % 3 === 0) : echo '<div class="row">'; endif; ?>
+                    <div class="col-sm-4">
+                        <a href="<?php the_permalink(); ?>">
+                            <div class="subject img-overlay">
 
-                      <div class="col-sm-4">
-                          <a href="<?php the_permalink(); ?>">
-                              <div class="subject img-overlay">
+                                <?php
 
-                                  <?php
+                                if ( has_post_thumbnail() ) {
+                                    the_post_thumbnail('large');
+                                }
 
-                                    if ( has_post_thumbnail() ) {
-                                      the_post_thumbnail('large');
-                                    }
+                                ?>
 
-                                  ?>
+                                <div class="portfolio-overlay">
+                                    <div class="overlay-copy">
+                                        <div class="center">
+                                            <span class="underline">
+                                                <h5><?php the_title(); ?></h5>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
 
-                                  <div class="portfolio-overlay">
-                                      <div class="overlay-copy">
-                                          <div class="center">
-                                              <span class="underline">
-                                                  <h5><?php the_title(); ?></h5>
-                                              </span>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </a>
-                      </div>
+                <?php $counter++; if($counter % 3 === 0) : echo '</div>'; endif; ?>
 
-                  <?php $counter++; if($counter % 3 === 0) : echo '</div>'; endif; ?>
-
-              <?php endwhile; endif; wp_reset_postdata(); ?>
-    </div>
-  </section>
+            <?php endwhile; endif; wp_reset_postdata(); ?>
+        </div>
+    </section>
 
     <a href="/hire-me/"><button type="button" class="d-sm-none btn btn-primary btn-lg btn-block"><h3>Hire me <i class="fa fa-chevron-right" aria-hidden="true"></i></h3></button></a>
     <a href="/hire-me/"><button type="button" class="d-none d-sm-block btn btn-primary btn-lg btn-block"><h3>Hire me to work on your project <i class="fa fa-chevron-right" aria-hidden="true"></i></h3></button></a>
