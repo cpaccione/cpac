2102ef9bad991cd6736cde867648c396e6df17c6
Chris Paccione <cpaccione77@gmail.com>
2018-09-14 08:20:34 -0500

    Started Wedding page

---
+++ page-wedding.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+
+Template Name: Portfolio Template
+
+*/
+
+?>
+
+<?php get_header(); ?>
+
+    <section class="wedding-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <h1>Gwen &amp; Chris</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="main-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            Thanks for visitiing our RSVP page. If you think you can make it we would really appreciate if you can fill out the form to the right, or feel free to give us a call.
+
+            The wedding will take place on March 30th, 2019 at Aldie Mansion in Doylestown, PA.
+            
+          </div>
+          <div class="col-sm-6">
+
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+
+<?php get_footer(); ?>
